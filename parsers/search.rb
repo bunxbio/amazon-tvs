5b9deb6e7e5786d1c56efc6333308b58657e02a9
Bio Kenfi Gumilang <biokenfigumilang@gmail.com>
2021-08-21 03:43:58 +0700

    modify config and parser for search

--- parsers/search.rb
+++ parsers/search.rb
@@ -1,17 +1,40 @@
 nokogiri = Nokogiri.HTML(content)
 
-products = nokogiri.at_css('h2.a-size-mini')
+products = nokogiri.css('h2.a-size-mini')
 products.each do |product|
-	a_element = product['href']
-	url = URI.join('https://www.amazon.com', a_element).to_s
+	a_element = product.css('.a-link-normal').attr('href').text
+	url = URI.join('https://www.amazon.com', a_element)
 
 	pages << {
 		url: url,
 		page_type: 'products',
+		fetch_type: 'browser',
 		force_fetch: true,
 		vars: {
 			category: page['vars']['category'],
 			url: url
 		}
 	}
+end
+
+pagination_links = nokogiri.css('ul.a-pagination li')
+pagination_links.each do |link|
+	page_num = link.text.strip
+	if page_num =~ /[0-9]/
+		url = "https://www.amazon.com/s?k=LED+%26+LCD+TVs&i=electronics&rh=n%3A6459737011&page=#{page_num}&qid=1629491149&ref=sr_pg_#{page_num}",
+		pages << {
+			url: url,
+			page_type: 'search',
+			fetch_type: 'browser',
+			force_fetch: true,
+			method: "GET",
+			headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+			driver: {
+				code: "await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000); await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);"
+			},
+			vars: {
+				category: page['vars']['category']
+			}
+		}
+	end
 end
