ecb08076ca8f6d2d1e485ba0a8f3b6a9fbf29106
Bio Kenfi Gumilang <biokenfigumilang@gmail.com>
2021-08-21 00:42:16 +0700

    trying new seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,30 +1,30 @@
 pages << {
-  url: 'https://www.amazon.com/s?rh=n%3A6459737011&fs=true&ref=lp_6459737011_sar',
+  url: 'https://www.amazon.com/s?i=electronics&rh=n%3A6459737011&fs=true&qid=1629481136&ref=sr_pg_1',
   page_type: "listings",
   fetch_type: 'browser',
   force_fetch: true,
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  driver: {
-    name: "click see all results",
-    code: "await page.click('#a-page > div.a-container > div.a-row.apb-browse-two-col-center-pad > div.a-column.a-span12.aok-float-right.apb-browse-col-pad-left.apb-browse-two-col-center-margin-right > div:nth-child(12) > div.a-section.a-spacing-none > div > div > a'); await sleep(3000);"
-  },
+#  driver: {
+#    name: "click see all results",
+#    pre_code: "await page.click('#a-page > div.a-container > div.a-row.apb-browse-two-col-center-pad > div.a-column.a-span12.aok-float-right.apb-browse-col-pad-left.apb-browse-two-col-center-margin-right > div:nth-child(12) > div.a-section.a-spacing-none > div > div > a'); await sleep(3000);"
+#  },
   vars: {
     category: "LED & LCD TVs"
   }
 }
 
 pages << {
-  url: 'https://www.amazon.com/s?rh=n%3A6463520011&fs=true&ref=lp_6463520011_sar',
+  url: 'https://www.amazon.com/s?i=electronics&rh=n%3A6463520011&fs=true&qid=1629480984&ref=sr_pg_1',
   page_type: "listings",
   fetch_type: 'browser',
   force_fetch: true,
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  driver: {
-    name: "click see all results",
-    code: "await page.click('#a-page > div.a-container > div.a-row.apb-browse-two-col-center-pad > div.a-column.a-span12.aok-float-right.apb-browse-col-pad-left.apb-browse-two-col-center-margin-right > div:nth-child(12) > div.a-section.a-spacing-none > div > div > a'); await sleep(3000);"
-  },
+#  driver: {
+#    name: "click see all results",
+#    pre_code: "await page.click('#a-page > div.a-container > div.a-row.apb-browse-two-col-center-pad > div.a-column.a-span12.aok-float-right.apb-browse-col-pad-left.apb-browse-two-col-center-margin-right > div:nth-child(12) > div.a-section.a-spacing-none > div > div > a'); await sleep(3000);"
+#  },
   vars: {
     category: "OLED TVs"
   }
