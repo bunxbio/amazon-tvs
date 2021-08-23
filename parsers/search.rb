659d93b4e98d0b66b974b0eae0fea13de877d3f3
bunxbio <biokenfigumilang@gmail.com>
2021-08-23 21:58:59 +0700

    modify parser for search to be more efficient and add limit to 10 pages for pagination

--- parsers/search.rb
+++ parsers/search.rb
@@ -4,7 +4,7 @@ products = nokogiri.css('h2.a-size-mini')
 products.each do |product|
 	a_element = product.at_css('.a-link-normal')
 	if a_element
-	url = URI.join('https://www.amazon.com', a_element['href'])
+	url = "https://www.amazon.com#{a_element['href']}"
 	pages << {
 		url: url,
 		page_type: 'products',
@@ -18,14 +18,37 @@ products.each do |product|
 end
 
 
-pagination_links = nokogiri.at_css('.a-last > a')
-if pagination_links
-	next_page = "https://www.amazon.com#{pagination_links['href']}"
-	pages << {
-		url: next_page,
-		page_type: 'search',
-		vars: {
-			category: page['vars']['category']
+current_page = nokogiri.at_css('li.a-selected')
+if current_page
+	current_page = current_page.text.to_i
+elsif !current_page
+	current_page = nokogiri.at_css('span.s-pagination-item.s-pagination-selected').text.to_i
+end
+
+if current_page < 11
+	pagination_links = nokogiri.at_css('.a-last > a')
+	if pagination_links
+		next_page = "https://www.amazon.com#{pagination_links['href']}"
+		pages << {
+			url: next_page,
+			page_type: 'search',
+			force_fetch: true,
+			vars: {
+				category: page['vars']['category']
+			}
 		}
-	}
+	elsif !pagination_links
+		pagination_links = nokogiri.at_css('.s-pagination-item.s-pagination-next.s-pagination-button')
+		if pagination_links
+			next_page = "https://www.amazon.com#{pagination_links['href']}"
+			pages << {
+				url: next_page,
+				page_type: 'search',
+				force_fetch: true,
+				vars: {
+					category: page['vars']['category']
+				}
+			}
+		end
+	end
 end
